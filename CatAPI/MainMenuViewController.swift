f3c8d0276c231ec3fbaff3186b0268449be81b1b
Pavel <pavel.m.3107@gmail.com>
2020-05-17 17:33:24 +0300

    Fixed Code and segues

---
+++ CatAPI/MainMenuViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainMenuViewController.swift
+//  CatAPI
+//
+//  Created by Pavel Ronaldo on 5/17/20.
+//  Copyright © 2020 Pavel Ronaldo. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func unwindMainMenu(_ segue: UIStoryboardSegue) {}
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
